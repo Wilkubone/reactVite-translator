68d66122a668050eccfd05bb0fac3f5af22be395
Tomek Wilczynski <106587849+Wilkubone@users.noreply.github.com>
2024-04-25 21:30:01 +0200

    new files

--- translator/src/app/App.tsx
+++ translator/src/app/App.tsx
@@ -1,3 +1,15 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../lib/styles";
 
-export const App = () => <div>Hello world!!!!</div>;
+export const App = () => (
+    <ThemeProvider theme={theme}>
+        <AppContainer>Hello world!!</AppContainer>
+    </ThemeProvider>
+);
+
+const AppContainer = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.background};
+`;
