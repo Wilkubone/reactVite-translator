1fa8fab99e42200d702f88bc24979eb52507345f
Tomek Wilczynski <106587849+Wilkubone@users.noreply.github.com>
2024-06-16 21:43:44 +0200

    poprawa pliku Loader?

--- translator/src/lib/components/Loader.tsx
+++ translator/src/lib/components/Loader.tsx
@@ -1,38 +1,82 @@
-import React from "react"
-import styled from "styled-components"
+// import React from "react"
+// import styled from "styled-components"
 
-export const Loader: React.FunctionComponent = ({ children }) => (
-    <LoaderContainer>
-        <ActivityIndicator/>
-        {children && (
-            <ChildrenContainer>
-            {children}
-        </ChildrenContainer>
-        ) }
-    </LoaderContainer>
-)
+// export const Loader: React.FunctionComponent = ({ children }) => (
+//     <LoaderContainer>
+//         <ActivityIndicator/>
+//         {children && (
+//             <ChildrenContainer>
+//             {children}
+//         </ChildrenContainer>
+//         ) }
+//     </LoaderContainer>
+// )
+
+// const ActivityIndicator = styled.div`
+//     width: 100%;
+//     height: 2px;
+//     background-color: ${({ theme }) => theme.colors.primary};
+//     border-radius: 6px;
+//     animation: loading 1s linear infinite alternate;
+
+//     @keyframes loading2 {
+//         0% {
+//             width: 0%;
+//         }
+
+//         100% {
+//             width: 100%;
+//         }
+//     }
+// `
+
+// const ChildrenContainer = styled.div`
+//     text-align: center;
+//     `
+//     const LoaderContainer = styled.div`
+//         width: 100%;
+//     `
+
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+interface LoaderProps {
+  children?: ReactNode;
+}
+
+export const Loader: React.FunctionComponent<LoaderProps> = ({ children }) => (
+  <LoaderContainer>
+    <ActivityIndicator />
+    {children && (
+      <ChildrenContainer>
+        {children}
+      </ChildrenContainer>
+    )}
+  </LoaderContainer>
+);
 
 const ActivityIndicator = styled.div`
-    width: 100%;
-    height: 2px;
-    background-color: ${({ theme }) => theme.colors.primary};
-    border-radius: 6px;
-    animation: loading 1s linear infinite alternate;
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 6px;
+  animation: loading 1s linear infinite alternate;
 
-    @keyframes loading2 {
-        0% {
-            width: 0%;
-        }
-
-        100% {
-            width: 100%;
-        }
+  @keyframes loading2 {
+    0% {
+      width: 0%;
     }
-`
+
+    100% {
+      width: 100%;
+    }
+  }
+`;
 
 const ChildrenContainer = styled.div`
-    text-align: center;
-    `
-    const LoaderContainer = styled.div`
-        width: 100%;
-    `
+  text-align: center;
+`;
+
+const LoaderContainer = styled.div`
+  width: 100%;
+`;
